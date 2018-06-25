0b69f33d75049e292de22208c5cc6b7d7a529530
wen_dzh <wen_dzh@163.com>
2018-06-25 17:02:03 +0800

    wrote all

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Display(Name = "电影名称")]
+        public string Title { get; set; }
+        [Display(Name = "发布日期"), DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Display(Name = "地区")]
+        public string Genre { get; set; }
+        [Display(Name = "评分"), Range(1, 100)]
+        public decimal Price { get; set; }
+    }
+}
